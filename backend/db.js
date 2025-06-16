24f24d180c4c3344d4961a595964e8505e7cee57
jzamm02 <161364556+jzamm02@users.noreply.github.com>
2025-06-16 14:00:42 +0200

    Local Postgres Setup
    Setup local postgres server instead of railway.

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+module.exports = pool;
---
+++ backend/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const pool = require("./db");
+
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+app.get("/movies", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM movies");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// add a movie to movies
+app.post("/movies", async (req, res) => {
+  console.log("Received request to add movie:", req.body);
+  try {
+    const { movie_key, name, description, genres, rate, length, img } =
+      req.body;
+    const result = await pool.query(
+      "INSERT INTO movies (movie_key, name, description, genres, rate, length, img) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [movie_key, name, description, genres, rate, length, img]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// delete movie
+app.delete("/movies/:id", async (req, res) => {
+  console.log("DELETE route hit with ID:", req.params.id);
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      "DELETE FROM movies WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).send("Movie not found");
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port http://localhost:3000");
+});
